dd63a04df9ecda83a36d4aff4a973736e172ff4c
gurhank <gurhankocyigit@gmail.com>
2021-07-10 11:59:00 -0500

    first scenario

--- src/test/java/com/cybertek/step_definitions/Google_StepDefinitions.java
+++ src/test/java/com/cybertek/step_definitions/Google_StepDefinitions.java
@@ -10,6 +10,14 @@ import org.openqa.selenium.Keys;
 
 public class Google_StepDefinitions {
 
+    @Then("User should see title is Google")
+    public void user_should_see_title_is_google() {
+        String actualTitle = Driver.getDriver().getTitle();
+        String expectedTitle = "Google";
+
+        Assert.assertTrue(actualTitle.equals(expectedTitle));
+    }
+
     @Given("User is on Google home page")
     public void user_is_on_google_home_page() {
 
