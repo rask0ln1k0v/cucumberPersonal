90ae7d1033931959a468acfa037a3744ca0982e5
gurhank <gurhankocyigit@gmail.com>
2021-07-17 13:21:12 -0500

    3rd day - take screenshot

--- src/test/java/com/cybertek/step_definitions/Hooks.java
+++ src/test/java/com/cybertek/step_definitions/Hooks.java
@@ -1,10 +1,7 @@
 package com.cybertek.step_definitions;
 
 import com.cybertek.utilities.Driver;
-import io.cucumber.java.After;
-import io.cucumber.java.AfterStep;
-import io.cucumber.java.Before;
-import io.cucumber.java.BeforeStep;
+import io.cucumber.java.*;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
@@ -22,12 +19,22 @@ public class Hooks {
     }
 
     @After
-    public void tearDownScenario(){
-        System.out.println("AFTER--Teardown steps are being applied...");
-        //Driver.closeDriver();
+    public void tearDownScenario(Scenario scenario){
+
+        //IF MY SCENARIO FAILS
+            // TAKE A SCREENSHOT
+
+        //scenario.isFailed()
 
         byte [] screenshot = ((TakesScreenshot)Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+        scenario.attach(screenshot, "image/png", scenario.getName());
 
+
+
+
+
+        System.out.println("AFTER--Teardown steps are being applied...");
+        //Driver.closeDriver();
     }
 
     @BeforeStep
