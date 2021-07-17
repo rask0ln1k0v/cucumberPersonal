0662363576ff3c0665d662d19da1a9535babeb82
gurhank <gurhankocyigit@gmail.com>
2021-07-17 11:25:31 -0500

    3nd day - cucumber - etsy example

--- src/test/java/com/cybertek/step_definitions/Etsy_StepDefinitions.java
+++ src/test/java/com/cybertek/step_definitions/Etsy_StepDefinitions.java
@@ -4,16 +4,24 @@ import com.cybertek.utilities.ConfigurationReader;
 import com.cybertek.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import org.junit.Assert;
 
 public class Etsy_StepDefinitions {
 
     @Given("user is on the Etsy landing page")
     public void user_is_on_the_etsy_landing_page() {
+
         String url = ConfigurationReader.getProperty("etsyUrl");
         Driver.getDriver().get(url);
+
     }
     @Then("user should see Etsy title as expected")
     public void user_should_see_etsy_title_as_expected() {
+
+        String expectedTitle = "Etsy - Shop for handmade, vintage, custom, and unique gifts for everyone";
+        String actualTitle = Driver.getDriver().getTitle();
+
+        Assert.assertTrue("Title is not as expected!",actualTitle.equals(expectedTitle));
 
     }
 
