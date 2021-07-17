56f5607032de1b36b448f8b16b2810dee66852e3
gurhank <gurhankocyigit@gmail.com>
2021-07-17 14:22:32 -0500

    3rd day - wikipedia practices

--- src/test/java/com/cybertek/step_definitions/Wiki_StepDefinitions.java
+++ src/test/java/com/cybertek/step_definitions/Wiki_StepDefinitions.java
@@ -1,5 +1,7 @@
 package com.cybertek.step_definitions;
 
+import com.cybertek.utilities.ConfigurationReader;
+import com.cybertek.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -8,23 +10,20 @@ public class Wiki_StepDefinitions {
 
     @Given("User is on Wikipedia home page")
     public void user_is_on_wikipedia_home_page() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        String url = ConfigurationReader.getProperty("wikiUrl");
+        Driver.getDriver().get(url);
     }
     @When("User types Steve Jobs in the wiki search box")
     public void user_types_steve_jobs_in_the_wiki_search_box() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
     @When("User clicks wiki search button")
     public void user_clicks_wiki_search_button() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
     @Then("User sees Steve Jobs is in the wiki title")
     public void user_sees_steve_jobs_is_in_the_wiki_title() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
 
 
