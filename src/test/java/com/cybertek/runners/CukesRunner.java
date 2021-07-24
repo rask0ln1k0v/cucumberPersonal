c84df5206c691ee57c11efa99dfc69fa6c2e0047
gurhank <gurhankocyigit@gmail.com>
2021-07-24 13:24:21 -0500

    5th day - json plugin

--- src/test/java/com/cybertek/runners/CukesRunner.java
+++ src/test/java/com/cybertek/runners/CukesRunner.java
@@ -7,7 +7,10 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = "html:target/cucumber-report.html",
+        plugin = {
+                "html:target/cucumber-report.html",
+                "json:target/cucumber.json"
+        },
         features = "src/test/resources/features",
         glue = "com/cybertek/step_definitions",
         dryRun = false,
