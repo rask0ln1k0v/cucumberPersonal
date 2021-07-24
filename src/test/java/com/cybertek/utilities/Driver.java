c1341ee5186089878eca1536979c726207017945
gurhank <gurhankocyigit@gmail.com>
2021-07-24 15:38:26 -0500

    5th day - Driver with ThreadLocal

--- src/test/java/com/cybertek/utilities/Driver.java
+++ src/test/java/com/cybertek/utilities/Driver.java
@@ -19,14 +19,14 @@ public class Driver {
     Making our 'driver' instance private so that it is not reachable from outside of the class.
     We make it static, because we want it to run before everything else, and also we will use it in a static method
      */
-    private static WebDriver driver;
+    private static ThreadLocal<WebDriver> driverPool = new ThreadLocal<>();
 
     /*
     Creating re-usable utility method that will return same 'driver' instance everytime we call it.
      */
     public static WebDriver getDriver(){
 
-        if (driver == null){
+        if (driverPool.get() == null){
 
             /*
             We read our browser type from configuration.properties file using
@@ -41,15 +41,15 @@ public class Driver {
             switch (browserType){
                 case "chrome":
                     WebDriverManager.chromedriver().setup();
-                    driver = new ChromeDriver();
-                    driver.manage().window().maximize();
-                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    driverPool.set(new ChromeDriver());
+                    driverPool.get().manage().window().maximize();
+                    driverPool.get().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
                     break;
                 case "firefox":
                     WebDriverManager.firefoxdriver().setup();
-                    driver = new FirefoxDriver();
-                    driver.manage().window().maximize();
-                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    driverPool.set(new FirefoxDriver());
+                    driverPool.get().manage().window().maximize();
+                    driverPool.get().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
                     break;
             }
         }
@@ -57,7 +57,7 @@ public class Driver {
         /*
         Same driver instance will be returned every time we call Driver.getDriver(); method
          */
-        return driver;
+        return driverPool.get();
 
 
     }
