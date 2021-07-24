e9ab8541ab1eed3bf67fedba9e7263c3a9c43d06
gurhank <gurhankocyigit@gmail.com>
2021-07-24 10:32:13 -0500

    5th day - practices

--- src/test/java/com/cybertek/step_definitions/DataTables_StepDefinitions.java
+++ src/test/java/com/cybertek/step_definitions/DataTables_StepDefinitions.java
@@ -14,6 +14,25 @@ public class DataTables_StepDefinitions {
 
     LibraryLoginPage loginPage = new LibraryLoginPage();
 
+
+    @Given("User is on the dropdowns page of practice tool")
+    public void user_is_on_the_dropdowns_page_of_practice_tool() {
+        Driver.getDriver().get("http://practice.cybertekschool.com/dropdown");
+    }
+    @Then("User should see below info in month dropdown")
+    public void user_should_see_below_info_in_month_dropdown(io.cucumber.datatable.DataTable dataTable) {
+        // Write code here that turns the phrase above into concrete actions
+        // For automatic transformation, change DataTable to one of
+        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+        // Double, Byte, Short, Long, BigInteger or BigDecimal.
+        //
+        // For other transformations you can register a DataTableType.
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+
     @Given("user is on the login page of library app")
     public void user_is_on_the_login_page_of_library_app() {
 
