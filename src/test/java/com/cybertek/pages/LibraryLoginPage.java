69ec9e406d191927ee155d67d5b28f4f7cbd540b
gurhank <gurhankocyigit@gmail.com>
2021-07-24 11:36:06 -0500

    5th day - creating utility method

--- src/test/java/com/cybertek/pages/LibraryLoginPage.java
+++ src/test/java/com/cybertek/pages/LibraryLoginPage.java
@@ -1,5 +1,6 @@
 package com.cybertek.pages;
 
+import com.cybertek.utilities.ConfigurationReader;
 import com.cybertek.utilities.Driver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -19,5 +20,29 @@ public class LibraryLoginPage {
 
     @FindBy(xpath = "//button[.='Sign in']")
     public WebElement signIn;
+
+    /*
+    Login method that logs in with specific username
+     */
+    public void login(){
+        emailInput.sendKeys("username");
+        passwordInput.sendKeys("password");
+        signIn.click();
+    }
+
+    /*
+    Login with admin
+     */
+    public void loginWithAdmin(){
+        emailInput.sendKeys("admin");
+        passwordInput.sendKeys("password");
+        signIn.click();
+    }
+
+    public void loginWithConfig(){
+        emailInput.sendKeys(ConfigurationReader.getProperty("username"));
+        passwordInput.sendKeys(ConfigurationReader.getProperty("password"));
+        signIn.click();
+    }
 
 }
