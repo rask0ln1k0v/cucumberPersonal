d3c0c26ba96e01f7d4d3914b01a3b81158bf63cd
gurhank <gurhankocyigit@gmail.com>
2021-07-25 11:36:33 -0500

    APACHE POI - excel write

--- src/test/java/com/cybertek/tests/ExcelWrite.java
+++ src/test/java/com/cybertek/tests/ExcelWrite.java
@@ -7,6 +7,7 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.Test;
 
 import java.io.FileInputStream;
+import java.io.IOException;
 
 public class ExcelWrite {
 
@@ -16,12 +17,25 @@ public class ExcelWrite {
     XSSFCell cell;
 
     @Test
-    public void excel_writing_test(){
+    public void excel_writing_test() throws IOException {
 
         String path = "SampleData.xlsx";
 
         //To be able to read from Excel file, we need to load it into FileInputStream
         FileInputStream fileInputStream = new FileInputStream(path);
+
+        //workbook > sheet > row > cell
+        workbook = new XSSFWorkbook(fileInputStream);
+
+        // get the sheet
+        sheet = workbook.getSheet("Employees");
+
+        //king's row
+        row = sheet.getRow(1);
+
+        //king's cell
+        cell = row.getCell(1);
+
 
 
     }
