c3e92846a62eac769cbe8a8c7f2bbb9e3d33837a
gurhank <gurhankocyigit@gmail.com>
2021-07-25 09:48:20 -0500

    APACHE POI - excel read

---
+++ src/test/java/com/cybertek/tests/ExcelRead.java
@@ -0,0 +1,16 @@
+package com.cybertek.tests;
+
+import org.junit.Test;
+
+public class ExcelRead {
+
+    /*
+    In this method we will learn how to read from excel file step by step
+     */
+    @Test
+    public void read_from_excel_file(){
+
+    }
+
+
+}
