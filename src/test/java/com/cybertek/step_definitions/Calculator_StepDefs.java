654e4405ed5e16a62b90ba052796b4d426e8b088
Mert <mertozer3@yandex.com>
2022-04-21 22:06:51 -0400

    vsCommit

--- src/test/java/com/cybertek/step_definitions/Calculator_StepDefs.java
+++ src/test/java/com/cybertek/step_definitions/Calculator_StepDefs.java
@@ -15,6 +15,7 @@ CalculatorPage calculatorPage = new CalculatorPage();
     @Given("User is on calculator page")
     public void user_is_on_calculator_page() {
         Driver.getDriver().get(ConfigurationReader.getProperty("calculator.url"));
+        //command
     }
     @Given("User clicks on {string} on calculator")
     public void user_clicks_on_on_calculator(String button) {
